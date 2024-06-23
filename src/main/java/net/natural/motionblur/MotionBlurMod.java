8ada2bf3653108c9d9688e8a36fd7302c88e5b65
ItsPasi <pasi123567@hotmail.com>
2024-06-24 00:48:21 +0200

    added new blur algorithm added config error handling

--- src/main/java/net/natural/motionblur/MotionBlurMod.java
+++ src/main/java/net/natural/motionblur/MotionBlurMod.java
@@ -12,6 +12,7 @@ import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
@@ -33,25 +34,20 @@ public class MotionBlurMod implements ClientModInitializer {
     private static MotionBlurConfig config;
     private static KeyBinding toggleKeybinding;
     private static final Gson GSON = new GsonBuilder().setLenient().setPrettyPrinting().create();
-    private static final ManagedShaderEffect motionblur = ShaderEffectManager.getInstance().manage(Identifier.of(ID, "shaders/post/motion_blur.json"),
-            shader -> shader.setUniformValue("BlendFactor", config.motionBlurStrength * 2));
+    private static final ManagedShaderEffect motionblur = ShaderEffectManager.getInstance().manage(
+            Identifier.of(ID, "shaders/post/motion_blur.json"),
+            shader -> shader.setUniformValue("BlendFactor", config.motionBlurStrength)
+    );
+    public enum BlurAlgorithm {BACKWARDS, CENTERED}
+    private static ClientTickEvents.EndTick endTickListener;
+    private static boolean configReset = false;
+    private static boolean delayMessageSent = false;
+    private static int tickCounter = 0;
+    private static final int TICK_DELAY = 80;
 
     @Override
     public void onInitializeClient() {
-        config = new MotionBlurConfig();
-
-        File configFile = FabricLoader.getInstance().getConfigDir().resolve("naturalmotionblur.json").toFile();
-        if(!configFile.exists()){
-            config = new MotionBlurConfig();
-            saveConfig();
-        }else{
-            try {
-                config = GSON.fromJson(FileUtils.readFileToString(configFile, StandardCharsets.UTF_8), MotionBlurConfig.class);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-        }
+        loadConfig();
 
         toggleKeybinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "Toggle Motion Blur",
@@ -65,54 +61,65 @@ public class MotionBlurMod implements ClientModInitializer {
             }
         });
 
-        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {dispatcher.register(ClientCommandManager.literal("motionblur")
-                .executes(context -> {
-                    ConfigBuilder builder = ConfigBuilder.create()
-                            .setParentScreen(null)
-                            .setTitle(Text.literal("Natural Motion Blur"));
-                    ConfigCategory general = builder.getOrCreateCategory(Text.literal("Motion Blur Options"));
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            dispatcher.register(ClientCommandManager.literal("motionblur")
+                    .executes(context -> {
+                        ConfigBuilder builder = ConfigBuilder.create()
+                                .setParentScreen(null)
+                                .setTitle(Text.literal("Natural Motion Blur"));
+                        ConfigCategory general = builder.getOrCreateCategory(Text.literal("Motion Blur Options"));
 
-                    ConfigEntryBuilder entryBuilder = builder.entryBuilder();
+                        ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
-                    general.addEntry(entryBuilder.startBooleanToggle(Text.literal("Toggle Motion Blur"), config.enabled)
-                            .setDefaultValue(true)
-                            .setSaveConsumer(newValue -> config.enabled = newValue)
-                            .build());
+                        general.addEntry(entryBuilder.startBooleanToggle(Text.literal("Toggle Motion Blur"), config.enabled)
+                                .setDefaultValue(true)
+                                .setSaveConsumer(newValue -> config.enabled = newValue)
+                                .build());
 
-                    general.addEntry(entryBuilder.startBooleanToggle(Text.literal("Third Person Rendering"), config.renderF5)
-                            .setDefaultValue(true)
-                            .setTooltip(Text.literal("Decide whether the motion blur should be rendered in third person (F5) or not."))
-                            .setSaveConsumer(newValue -> config.renderF5 = newValue)
-                            .build());
+                        general.addEntry(entryBuilder.startBooleanToggle(Text.literal("Third Person Rendering"), config.renderF5)
+                                .setDefaultValue(true)
+                                .setTooltip(Text.literal("Decide whether the motion blur should be rendered in third person (F5) or not."))
+                                .setSaveConsumer(newValue -> config.renderF5 = newValue)
+                                .build());
 
-                    general.addEntry(entryBuilder.startFloatField(Text.literal("Motion Blur Strength"), config.motionBlurStrength)
-                            .setDefaultValue(1.0F)
-                            .setTooltip(Text.literal("Sets the intensity of the blur. Default setting (1.0) blends frames ideally in correlation to framerate"))
-                            .setSaveConsumer(newValue -> config.motionBlurStrength = newValue)
-                            .build());
+                        general.addEntry(entryBuilder.startFloatField(Text.literal("Motion Blur Strength"), config.motionBlurStrength)
+                                .setDefaultValue(1.0F)
+                                .setTooltip(Text.literal("Sets the intensity of the blur. \n" +
+                                        "Default setting (1.0) blends frames ideally in correlation to framerate."))
+                                .setSaveConsumer(newValue -> config.motionBlurStrength = newValue)
+                                .build());
 
-                    general.addEntry(entryBuilder.startIntField(Text.literal("Motion Blur Sample Amount"), config.motionBlurSamples)
-                            .setDefaultValue(20)
-                            .setTooltip(Text.literal("Higher values improve visual appearance (especially at lower FPS) but impact performance negatively."))
-                            .setSaveConsumer(newValue -> config.motionBlurSamples = newValue)
-                            .build());
+                        general.addEntry(entryBuilder.startIntField(Text.literal("Motion Blur Sample Amount"), config.motionBlurSamples)
+                                .setDefaultValue(20)
+                                .setTooltip(Text.literal("Higher values improve visual appearance (especially at lower FPS) but impact performance negatively."))
+                                .setSaveConsumer(newValue -> config.motionBlurSamples = newValue)
+                                .build());
 
+                        general.addEntry(entryBuilder.startEnumSelector(
+                                        Text.literal("Blur Algorithm"),
+                                        BlurAlgorithm.class,
+                                        config.blurAlgorithm)
+                                .setDefaultValue(BlurAlgorithm.CENTERED)
+                                .setTooltip(Text.literal("Changes the blur to either only blend frames backwards or in both directions. \n\n" +
+                                        "BACKWARDS has better blur continuity (better frame blending). \n" +
+                                        "CENTERED has better visual uniformity (e.g. translucent objects) and less perceived input lag."))
+                                .setSaveConsumer(newValue -> config.blurAlgorithm = newValue)
+                                .build());
 
-                    general.addEntry(entryBuilder.startKeyCodeField(Text.literal("Toggle Key"), InputUtil.fromKeyCode(config.toggleKey, 0))
-                            .setDefaultValue(ModifierKeyCode.of(InputUtil.fromTranslationKey("key.keyboard.v"), Modifier.none()))
-                            .setKeySaveConsumer(newValue -> {
-                                config.toggleKey = newValue.getCode();
-                                toggleKeybinding.setBoundKey(newValue);
-                                KeyBinding.updateKeysByCode();
-                                MinecraftClient.getInstance().options.write();
-                            })
-                            .build());
-                    builder.setSavingRunnable(this::saveConfig);
-                    MinecraftClient.getInstance().send(() ->
-                            MinecraftClient.getInstance().setScreen(builder.build())
-                    );
-                    return 1;
-                }));
+                        general.addEntry(entryBuilder.startKeyCodeField(Text.literal("Toggle Key"), InputUtil.fromKeyCode(config.toggleKey, 0))
+                                .setDefaultValue(ModifierKeyCode.of(InputUtil.fromTranslationKey("key.keyboard.v"), Modifier.none()))
+                                .setKeySaveConsumer(newValue -> {
+                                    config.toggleKey = newValue.getCode();
+                                    MinecraftClient.getInstance().options.setKeyCode(toggleKeybinding, newValue);
+                                    KeyBinding.updateKeysByCode();
+                                })
+                                .build());
+                        builder.setSavingRunnable(this::saveConfig);
+                        MinecraftClient.getInstance().send(() ->
+                                MinecraftClient.getInstance().setScreen(builder.build())
+                        );
+                        return 1;
+                    }));
             dispatcher.register(ClientCommandManager.literal("mb").executes(context -> {
                 return dispatcher.execute("motionblur", context.getSource());
             }));
@@ -121,17 +128,53 @@ public class MotionBlurMod implements ClientModInitializer {
         PostWorldRenderCallbackV2.EVENT.register((matrix, camera, deltaTick) -> {
             if (config.motionBlurStrength != 0 && config.enabled) {
                 if (currentBlur != config.motionBlurStrength) {
-                    motionblur.setUniformValue("BlendFactor", config.motionBlurStrength * 2);
+                    motionblur.setUniformValue("BlendFactor", config.motionBlurStrength);
                     currentBlur = config.motionBlurStrength;
                 }
                 motionblur.setUniformValue("view_res", (float) MinecraftClient.getInstance().getFramebuffer().viewportWidth, (float) MinecraftClient.getInstance().getFramebuffer().viewportHeight);
                 motionblur.setUniformValue("view_pixel_size", 1.0f / MinecraftClient.getInstance().getFramebuffer().viewportWidth, 1.0f / MinecraftClient.getInstance().getFramebuffer().viewportHeight);
                 motionblur.setUniformValue("motionBlurSamples", config.motionBlurSamples);
+                motionblur.setUniformValue("blurAlgorithm", config.blurAlgorithm.ordinal());
                 if(!MinecraftClient.getInstance().options.getPerspective().isFirstPerson() && !config.renderF5) return;
 
                 motionblur.render(deltaTick);
             }
         });
+
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+            if (configReset) {
+                tickCounter = 0;
+                delayMessageSent = false;
+                endTickListener = tickClient -> {
+                    if (tickCounter >= TICK_DELAY && !delayMessageSent) {
+                        if (client.player != null) {
+                            client.player.sendMessage(Text.literal("§cNatural Motion Blur has encountered an issue and has been reset to its default settings."), false);
+                            delayMessageSent = true; // Mark message as sent
+                        }
+                    } else {
+                        tickCounter++;
+                    }
+                };
+                ClientTickEvents.END_CLIENT_TICK.register(endTickListener);
+            }
+        });
+    }
+
+    private void loadConfig() {
+        File configFile = FabricLoader.getInstance().getConfigDir().resolve("naturalmotionblur.json").toFile();
+        if (!configFile.exists()) {
+            config = new MotionBlurConfig();
+            saveConfig();
+        } else {
+            try {
+                config = GSON.fromJson(FileUtils.readFileToString(configFile, StandardCharsets.UTF_8), MotionBlurConfig.class);
+                config.blurAlgorithm = BlurAlgorithm.values()[config.blurAlgorithm.ordinal()];
+            } catch (Exception e) {
+                config = new MotionBlurConfig();
+                saveConfig();
+                configReset = true;
+            }
+        }
     }
 
     private void saveConfig() {
@@ -142,7 +185,6 @@ public class MotionBlurMod implements ClientModInitializer {
             throw new RuntimeException(e);
         }
     }
-
 
     public static void setFrameMotionBlur(Matrix4f modelView, Matrix4f prevModelView, Matrix4f projection, Matrix4f prevProjection, Vector3f cameraPos, Vector3f prevCameraPos) {
         motionblur.setUniformValue("modelView", new Matrix4f(modelView));
--- src/main/java/net/natural/motionblur/config/MotionBlurConfig.java
+++ src/main/java/net/natural/motionblur/config/MotionBlurConfig.java
@@ -1,15 +1,16 @@
 package net.natural.motionblur.config;
 
 
+import net.natural.motionblur.MotionBlurMod;
 import org.lwjgl.glfw.GLFW;
 
 public class MotionBlurConfig {
 
     public float motionBlurStrength = 1.0F;
     public int motionBlurSamples = 20;
+    public MotionBlurMod.BlurAlgorithm blurAlgorithm = MotionBlurMod.BlurAlgorithm.CENTERED;
     public boolean renderF5 = true;
     public boolean enabled = true;
     public int toggleKey = GLFW.GLFW_KEY_V;
-
 
 }
