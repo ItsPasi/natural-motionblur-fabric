b5c60ede1a3725c9a841bcefaae49e318fde1ad5
ItsPasi <pasi123567@hotmail.com>
2024-07-03 12:25:31 +0200

    Added support for 1.19.4-1.20.4

--- src/main/java/net/natural/motionblur/mixin/MixinLevelRenderer.java
+++ src/main/java/net/natural/motionblur/mixin/MixinLevelRenderer.java
@@ -1,10 +1,11 @@
 package net.natural.motionblur.mixin;
 
-import net.natural.motionblur.MotionBlurMod;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.natural.motionblur.MotionBlurMod;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
@@ -20,13 +21,13 @@ public class MixinLevelRenderer {
     @Unique private Vector3f prevCameraPos = new Vector3f();
 
     @Inject(method = "render", at = @At("HEAD"))
-    private void setMatrices(float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f matrix4f, Matrix4f matrix4f2, CallbackInfo ci) {
-        MotionBlurMod.setFrameMotionBlur(matrix4f, prevModelView, gameRenderer.getBasicProjectionMatrix(((GameRendererInvoker) gameRenderer).invokeGetFov(camera, tickDelta, true)), prevProjection, new Vector3f((float) (camera.getPos().x % 30000f), (float) (camera.getPos().y % 30000f), (float) (camera.getPos().z % 30000f)), prevCameraPos);
+    private void setMatrices(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f projectionMatrix, CallbackInfo ci) {
+        MotionBlurMod.setFrameMotionBlur(matrices.peek().getPositionMatrix(), prevModelView, gameRenderer.getBasicProjectionMatrix(((GameRendererInvoker) gameRenderer).invokeGetFov(camera, tickDelta, true)), prevProjection, new Vector3f((float) (camera.getPos().x % 30000f), (float) (camera.getPos().y % 30000f), (float) (camera.getPos().z % 30000f)), prevCameraPos);
     }
 
     @Inject(method = "render", at = @At("RETURN"))
-    private void setOldMatrices(float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f matrix4f, Matrix4f matrix4f2, CallbackInfo ci) {
-        prevModelView = new Matrix4f(matrix4f);
+    private void setOldMatrices(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f projectionMatrix, CallbackInfo ci) {
+        prevModelView = new Matrix4f(matrices.peek().getPositionMatrix());
         prevProjection = new Matrix4f(gameRenderer.getBasicProjectionMatrix(((GameRendererInvoker) gameRenderer).invokeGetFov(camera, tickDelta, true)));
         prevCameraPos = new Vector3f((float) (camera.getPos().x % 30000f), (float) (camera.getPos().y % 30000f), (float) (camera.getPos().z % 30000f));
 
