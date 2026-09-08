302cba5afb6b92e2c08ae4238d8a069ffb41ad08
Pasi <pasi123567@hotmail.com>
2026-09-08 23:48:18 +0200

    - implement adjustable strength for frame blending

--- src/main/java/net/natural/motionblur/config/ConfigEntries.java
+++ src/main/java/net/natural/motionblur/config/ConfigEntries.java
@@ -13,7 +13,6 @@ public class ConfigEntries {
 
     public boolean usesVelocityBlur() {return blurAlgorithm == BlurAlgorithm.VELOCITY_BASED || blurAlgorithm == BlurAlgorithm.HYBRID_BLENDING;}
     public boolean allowsRefreshRateScaling() {return blurAlgorithm == BlurAlgorithm.VELOCITY_BASED;}
-    public boolean locksStrengthToOne() {return blurAlgorithm == BlurAlgorithm.HYBRID_BLENDING;}
-    public float getEffectiveMotionBlurStrength() {return locksStrengthToOne() ? 1.0F : motionBlurStrength;}
-    public boolean showsStrengthSlider() {return blurAlgorithm != BlurAlgorithm.FRAME_BLENDING && !locksStrengthToOne();}
+    public float getEffectiveMotionBlurStrength() {return motionBlurStrength;}
+    public boolean showsStrengthSlider() {return true;}
 }
